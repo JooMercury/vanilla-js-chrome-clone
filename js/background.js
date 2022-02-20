8d45319a058a2553b4ea9f127fd988d52426dce7
JooMercury <91738673+JooMercury@users.noreply.github.com>
2022-02-20 13:02:36 +0900

    update

--- js/background.js
+++ js/background.js
@@ -1,9 +1,24 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "0.jpeg",
+  "1.jpeg",
+  "2.jpeg",
+  "3.jpeg",
+  "4jpeg",
+  "5.jpeg",
+  "6.jpeg",
+  "7.jpeg",
+  "8.jpeg",
+  "9.jpeg",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+document.body.style.background = `url(img/${chosenImage})`;
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
